97d4883b057774aa4024c4ca93bbb35a1a592a0a
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-29 18:46:13 +0500

    first pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { WordCardComponentComponent } from "./components/word-card-component/word-card-component.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.less',
+    imports: [CommonModule, RouterOutlet, WordCardComponentComponent]
 })
 export class AppComponent {
   title = 'word-dictionary';
