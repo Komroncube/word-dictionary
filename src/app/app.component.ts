bada41bd02b0802b88655907e996ecefacd884a8
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-29 21:02:42 +0500

    seed data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { WordCardComponentComponent } from "./components/word-card-component/word-card-component.component";
+import { IWordCard, WordCardComponentComponent } from "./components/word-card-component/word-card-component.component";
 
 @Component({
     selector: 'app-root',
@@ -12,4 +12,77 @@ import { WordCardComponentComponent } from "./components/word-card-component/wor
 })
 export class AppComponent {
   title = 'word-dictionary';
+  words: IWordCard[] = [
+    
+    {
+      id: 1,
+      word: "apple",
+      translation: "olma",
+      example: "I enjoy eating a juicy apple.",
+      photo: "../assets/apple.jpg"
+    },
+      {
+      id: 2,
+      word: 'car',
+      translation: 'avtomobil',
+      example: 'He drives a fast and sleek car.',
+      photo: '../assets/car.jpg'
+      },
+      {
+      id: 3,
+      word: 'house',
+      translation: 'uy',
+      example: 'Our family lives in a beautiful house.',
+      photo: '../assets/house.jpg'
+      },
+      {
+      id: 4,
+      word: 'cat',
+      translation: 'mushuk',
+      example: 'The cat is sleeping on the windowsill.',
+      photo: '../assets/cat.jpg'
+      },
+      {
+      id: 5,
+      word: 'dog',
+      translation: 'it',
+      example: 'A loyal dog is a wonderful companion.',
+      photo: '../assets/dog.jpg'
+      },
+      {
+      id: 6,
+      word: 'sun',
+      translation: 'quyosh',
+      example: 'The sun rises in the east.',
+      photo: '../assets/sun.jpg'
+      },
+      {
+      id: 7,
+      word: 'moon',
+      translation: 'oy',
+      example: 'The moon shines brightly in the night sky.',
+      photo: '../assets/moon.jpg'
+      },
+      {
+      id: 8,
+      word: 'book',
+      translation: 'kitob',
+      example: 'I am reading an interesting book.',
+      photo: '../assets/book.png'
+      },
+      {
+      id: 9,
+      word: 'computer',
+      translation: 'kompyuter',
+      example: 'I use a computer for work and entertainment.',
+      photo: '../assets/computer.jpg'
+      },
+      {
+      id: 10,
+      word: 'tree',
+      translation: 'daraxt',
+      example: 'The birds are singing in the tree.',
+      photo: '../assets/tree.jpg'
+      }
+  ]
 }
