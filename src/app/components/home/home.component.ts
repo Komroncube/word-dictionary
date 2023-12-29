c5f636df4771cd2ed3824d07f40f61dd9ef1dde4
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-29 22:12:13 +0500

    searching text

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,77 +9,90 @@ import { IWordCard, WordCardComponentComponent } from "../word-card-component/wo
     imports: [WordCardComponentComponent]
 })
 export class HomeComponent {
-words: IWordCard[] = [
-    
-    {
-      id: 1,
-      word: "apple",
-      translation: "olma",
-      example: "I enjoy eating a juicy apple.",
-      photo: "../assets/apple.jpg"
-    },
+  words: IWordCard[] = [
+      
       {
-      id: 2,
-      word: 'car',
-      translation: 'avtomobil',
-      example: 'He drives a fast and sleek car.',
-      photo: '../assets/car.jpg'
+        id: 1,
+        word: "apple",
+        translation: "olma",
+        example: "I enjoy eating a juicy apple.",
+        photo: "../assets/apple.jpg"
       },
-      {
-      id: 3,
-      word: 'house',
-      translation: 'uy',
-      example: 'Our family lives in a beautiful house.',
-      photo: '../assets/house.jpg'
-      },
-      {
-      id: 4,
-      word: 'cat',
-      translation: 'mushuk',
-      example: 'The cat is sleeping on the windowsill.',
-      photo: '../assets/cat.jpg'
-      },
-      {
-      id: 5,
-      word: 'dog',
-      translation: 'it',
-      example: 'A loyal dog is a wonderful companion.',
-      photo: '../assets/dog.jpg'
-      },
-      {
-      id: 6,
-      word: 'sun',
-      translation: 'quyosh',
-      example: 'The sun rises in the east.',
-      photo: '../assets/sun.jpg'
-      },
-      {
-      id: 7,
-      word: 'moon',
-      translation: 'oy',
-      example: 'The moon shines brightly in the night sky.',
-      photo: '../assets/moon.jpg'
-      },
-      {
-      id: 8,
-      word: 'book',
-      translation: 'kitob',
-      example: 'I am reading an interesting book.',
-      photo: '../assets/book.png'
-      },
-      {
-      id: 9,
-      word: 'computer',
-      translation: 'kompyuter',
-      example: 'I use a computer for work and entertainment.',
-      photo: '../assets/computer.jpg'
-      },
-      {
-      id: 10,
-      word: 'tree',
-      translation: 'daraxt',
-      example: 'The birds are singing in the tree.',
-      photo: '../assets/tree.jpg'
-      }
-  ]
+        {
+        id: 2,
+        word: 'car',
+        translation: 'avtomobil',
+        example: 'He drives a fast and sleek car.',
+        photo: '../assets/car.jpg'
+        },
+        {
+        id: 3,
+        word: 'house',
+        translation: 'uy',
+        example: 'Our family lives in a beautiful house.',
+        photo: '../assets/house.jpg'
+        },
+        {
+        id: 4,
+        word: 'cat',
+        translation: 'mushuk',
+        example: 'The cat is sleeping on the windowsill.',
+        photo: '../assets/cat.jpg'
+        },
+        {
+        id: 5,
+        word: 'dog',
+        translation: 'it',
+        example: 'A loyal dog is a wonderful companion.',
+        photo: '../assets/dog.jpg'
+        },
+        {
+        id: 6,
+        word: 'sun',
+        translation: 'quyosh',
+        example: 'The sun rises in the east.',
+        photo: '../assets/sun.jpg'
+        },
+        {
+        id: 7,
+        word: 'moon',
+        translation: 'oy',
+        example: 'The moon shines brightly in the night sky.',
+        photo: '../assets/moon.jpg'
+        },
+        {
+        id: 8,
+        word: 'book',
+        translation: 'kitob',
+        example: 'I am reading an interesting book.',
+        photo: '../assets/book.png'
+        },
+        {
+        id: 9,
+        word: 'computer',
+        translation: 'kompyuter',
+        example: 'I use a computer for work and entertainment.',
+        photo: '../assets/computer.jpg'
+        },
+        {
+        id: 10,
+        word: 'tree',
+        translation: 'daraxt',
+        example: 'The birds are singing in the tree.',
+        photo: '../assets/tree.jpg'
+        }
+    ];
+    /**
+     * 
+     */
+  filteredWords: IWordCard[] = this.words 
+    /**
+     * 
+     * @param text searching text
+     */
+  search(text:string){
+    this.filteredWords = this.words.filter( word => word.word
+                                  .toLocaleLowerCase()
+                                  .includes(text.toLocaleLowerCase()))
+  }
 }
