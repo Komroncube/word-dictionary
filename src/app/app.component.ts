45fa4d25af4282a2c8ecd07b8f78ceda8bc50726
Abdujabborov Kamronbek <komronabdujabbor@gmail.com>
2023-12-30 12:59:17 +0500

    added routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { IWordCard, WordCardComponentComponent } from "./components/word-card-component/word-card-component.component";
+import { WordCardComponentComponent } from "./components/word-card-component/word-card-component.component";
 import { HomeComponent } from "./components/home/home.component";
+import { DetailsComponent } from "./components/details/details.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.less',
-    imports: [CommonModule, RouterOutlet, WordCardComponentComponent, HomeComponent]
+    imports: [CommonModule, RouterOutlet, WordCardComponentComponent, HomeComponent, DetailsComponent]
 })
 export class AppComponent {
   title = 'word-dictionary';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DetailsComponent } from './components/details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: HomeComponent
+    },
+    {
+        path:'details/:id',
+        component: DetailsComponent
+    }
+];
